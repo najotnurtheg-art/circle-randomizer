65f1a7596c3d648888281e5551a98ad9a80a2d1d
najotnurtheg-art <najotnurtheg@gmail.com>
2025-10-06 17:39:39 +0500

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main style={{ padding: 24, fontFamily: 'system-ui, sans-serif' }}>
+      <h1>Circle Randomizer</h1>
+      <p>It works! Next: add /register, /login, /wheel.</p>
+    </main>
+  );
+}
