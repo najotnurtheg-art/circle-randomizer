11b0122f1d4e91b13c948b111a29f54a7538bc44
najotnurtheg-art <najotnurtheg@gmail.com>
2025-10-06 19:05:16 +0500

    Create prisma.js

---
+++ app/lib/prisma.js
@@ -0,0 +1,3 @@
+import { PrismaClient } from '@prisma/client';
+export const prisma = globalThis._prisma || new PrismaClient();
+if (process.env.NODE_ENV !== 'production') globalThis._prisma = prisma;
