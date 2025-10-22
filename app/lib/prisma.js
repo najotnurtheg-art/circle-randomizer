0e2f586968fb5b3767fa910d5539bc98f11ec02e
najotnurtheg-art <najotnurtheg@gmail.com>
2025-10-22 21:15:06 +0500

    Update prisma.js

--- app/lib/prisma.js
+++ app/lib/prisma.js
@@ -1,9 +1,3 @@
 import { PrismaClient } from '@prisma/client';
-
-const globalForPrisma = globalThis;
-
-const prisma = globalForPrisma.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
-
-export default prisma;
+export const prisma = globalThis._prisma || new PrismaClient();
+if (process.env.NODE_ENV !== 'production') globalThis._prisma = prisma;
